e4a01664e687134ae5ec53c274119e75bfb34fe4
Aadarsh Yadav <aadarshyadav1250@gmail.com>
2025-07-30 01:31:14 +0530

    Initial commit - React + Vite + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-2xl bg-amber-200 p-4'>Hello Aadarsh Yadav ! Welcome in the personal portfolio website development journey </h1>
+    </>
+  )
+}
+
+export default App
