aa02749b914c45c5cfad73d6df8841da25f9044b
Aadarsh Yadav <aadarshyadav1250@gmail.com>
2025-07-30 02:19:16 +0530

    Navbar is created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import Navbar from './components/Navbar'
 
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
 
   return (
     <>
+      <Navbar />
       <h1 className='text-2xl bg-amber-200 p-4'>Hello Aadarsh Yadav ! Welcome in the personal portfolio website development journey </h1>
     </>
   )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+  <>
+    <div className='navbar bg-gray-200 font-bold'>
+        
+        <div className="navbar-links flex justify-between p-4">
+            <div className='navbar-logo text-3xl'>
+                <h2>Aadarsh Yadav</h2>
+            </div>
+            <ul className='flex items-center justify-between'>
+                <li><a className='hover:bg-gray-400 hover:text-white p-6' href='/about'>About</a></li>
+                <li><a className='hover:bg-gray-400 hover:text-white p-6' href='/skills'>Skills</a></li>
+                <li><a className='hover:bg-gray-400 hover:text-white p-6' href='/projects'>Projects</a></li>
+                <li><a className='hover:bg-gray-400 hover:text-white p-6' href='/contact'>Contact</a></li>
+                <li><a className='hover:bg-gray-400 hover:text-white p-6' href='/blogs'>Blogs</a></li>
+            </ul>
+        </div>
+    </div>
+
+  </>)
+}
+
+export default Navbar
